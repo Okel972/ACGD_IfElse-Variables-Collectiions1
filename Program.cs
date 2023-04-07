67c8647dace4810c0e7d4dc72ff06ffdc6c665fd
Kevin <akmtime@hotmail.com>
2023-04-08 01:11:56 +0200

    Envoi avec collection mais pas abouti

--- Program.cs
+++ Program.cs
@@ -18,10 +18,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
                     time = int.Parse(newtime);
 
-                    if (time < 1)
-                    {
-                        Console.WriteLine("Erreur : le nombre de mois doit être supérieur à 1");
-                    }
+                if (time < 0)
+                {
+                    Console.WriteLine("Erreur : le nombre de mois doit être supérieur à 1");
+                }
             }
             return time;
         }
@@ -75,7 +75,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Console.WriteLine("Afin de bien vous orienter, veuillez répondre aux questions suivantes :");
 
             bool test = false;
-            
+
             while (true)
             {
                 int time = DemanderTemps();
@@ -117,6 +117,43 @@ namespace MyApp // Note: actual namespace depends on the project name.
             int perimetre = (longueur + largeur) * 2;
 
             Console.WriteLine($"\nLe périmètre du rectangle est de {perimetre} cm2 et l'air est de {air} cm2 ");
+
+            Console.WriteLine("\nOK ! Très bien ! Enfin pour finaliser passons à la dernière étape");
+
+            Console.WriteLine("\nNous allons chercher à trouver un nombre dans une liste et calculer la somme de tous les nombres de celle-ci");
+
+            List<int> list = new List<int>();
+
+            Console.WriteLine("\nEntrer différents nombres entiers positifs");
+
+            Console.Write("\nPremier nombre positif : ");
+
+            int nb = int.Parse(Console.ReadLine());
+            list.Add(nb);
+
+            Console.Write("Deuxième nombre positif : ");
+
+            int nb2 = int.Parse(Console.ReadLine());
+            list.Add(nb2);
+
+            Console.Write("Troisième nombre positif : ");
+
+            int nb3 = int.Parse(Console.ReadLine());
+            list.Add(nb3);
+
+            foreach (int i in list) ;
+
+            int somme = nb + nb2 + nb3;
+
+            string str = "The greater of {list}.";
+
+            //int SommeTotale = max + somme;
+
+            Console.WriteLine($"La somme de la liste est égal à {somme}");
+
+            //Console.WriteLine(str, nb, nb2, nb3, Math.Max(nb, nb2, nb3));
+
+            //Console.WriteLine($"Voici la somme de tout les nombre : {SommeTotale}");
         }
     }
 }
